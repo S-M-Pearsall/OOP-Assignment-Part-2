8709ab99fd742ff353df0a4db86971b0e38e93cf
smpearsall <smpearsall@gmail.com>
2020-11-06 17:11:30 +1030

    Changing test file
    changing test file

--- test_file.py
+++
@@ -1,8 +0,0 @@
-from Assignment2Mastermind import MastermindOnePlayer
-#from Assignment2Mastermind import MastermindTwoPlayer
-import pytest
-
-def test_shield_code():
-    m = MastermindOnePlayer()
-    m.generateShieldCode()
-    assert len(m.secretCodeList) == 4
---
+++ test_file_pytest.py
@@ -0,0 +1,34 @@
+import pytest
+from Assignment2Mastermind import MastermindOnePlayer
+#from Assignment2Mastermind import MastermindTwoPlayer
+'''
+class test_Mastermind_One_Player:
+    def test_shield_code(self):
+        player1 = "Jesse"
+        m = MastermindOnePlayer(player1)
+        m.generateShieldCode()
+        assert len(m.secretCodeList) == 4
+
+'''
+
+
+
+'''
+class TestCelsius:
+    def test_temperature(self):
+        assert fahrenheit(27) == 27*(9/5)+32
+
+    def test_temperatureSecond(self):
+        assert fahrenheit(0) == 32
+
+    def test_temperatureThird(self):
+        assert fahrenheit(-4) == (-4)*(9/5)+32
+
+    def test_temperatureFourth(self):
+        with pytest.raises(TypeError): 
+            fahrenheit(True)
+
+    def test_something(self):
+        with pytest.raises(TypeError):
+            fahrenheit("34")
+'''
